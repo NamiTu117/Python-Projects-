c5cf62a2a2bb934a5e01aa8e0f61b09252e5a85f
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-09-27 21:52:45 -0400

    Update SQL Assignment.py

--- SQL Assignment.py
+++ SQL Assignment.py
@@ -1,11 +1,14 @@
 import sqlite3
 
+#creates connection 
 conn = sqlite3.connect('test_database.db')
 
+#creating table 
 with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT)")
     cur.execute("INSERT INTO People VALUES('Ron', 'Stoppable', 16)")
     conn.commit()
-    
-    
+
 
 
