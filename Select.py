34132d7692b1c4aaa397b26cb772d4e928770a52
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-09-30 13:09:16 -0400

    Checks

--- Select.py
+++ Select.py
@@ -13,21 +13,28 @@ from datetime import datetime, timedelta
 
 #creating function for user to browse and retrieve folders
 def Directory():
-     browse ="C:/Users/Nami/OneDrive/Documents/Python-Projects-/Working/"
-     browse2="C:/Users/Nami/OneDrive/Documents/Python-Projects-/Stationary/"
-     now = datetime.now()
-     time = now.strftime("24:00:00")
+     file = input("Open File (Y/N): ")
+     if file == "Y":
+          browse ="C:/Users/Nami/OneDrive/Documents/Python-Projects-/Files/"
+          print(os.listdir(browse))
+          file = input ("Select your file: ")
+          if file == "Working":
+               file1 = "C:/Users/Nami/OneDrive/Documents/Python-Projects-/Files/Working/"
+               print(os.listdir(file1))
+     elif file == "N":
+          print("Have a good day!") 
+     
      #Working has recent files. Stationary is to receive files
-     file = input("Working or Stationary: ")
-     if file == "Working":
-         print(os.listdir(browse))
-         file = input("\nSelect any file to see if it's been accessed/modified within the past 24hrs\n")
-         if file < time: 
-             print("This file was accessed/modified as of recent")
-         else:
-                 print("This file has not been accessed/modified within the past 24hrs")
-     elif file == "Stationary":
-         print(os.listdir(browse2))
+     
+
+     #    print(os.listdir(browse))
+      #   file = input("\nSelect any file to see if it's been accessed/modified within the past 24hrs\n")
+     #  if file < time: 
+      #       print("This file was accessed/modified as of recent")
+       #  else:
+        #         print("This file has not been accessed/modified within the past 24hrs")
+     #elif file == "Stationary":
+      #   print(os.listdir(browse2))
 
          #def Detect(Directory):
             # file = input("Select a file to see if it has been accessed/edited within the past 24hrs: ")
